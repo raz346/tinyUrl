cde79e7464bc5aa49b36bb3fa9929d60422ad4ce
raz346 <alhashimi346@gmail.com>
2019-09-02 19:43:14 +0000

    finish server initial setup

--- express_server.js
+++ express_server.js
@@ -12,7 +12,8 @@ app.get("/", (req, res) => {
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
